8578363d4e7f3f74b7bfc37a3b738725cc053d06
davidjocic93 <davidjocic93@gmail.com>
2017-10-10 15:37:15 +0200

    loopsf

--- loops/l2.js
+++ loops/l2.js
@@ -1,9 +1,5 @@
 var sum = 0;
-<<<<<<< HEAD
-for (var x = 0; x <= 1000; x++) {
-=======
 for (var x = 0; x < 1000; x++) {
->>>>>>> 24264f19f7f5198df4d35a476abd46d91c7b05cb
 
     if (x % 3 === 0 && x % 5 === 0) {
         sum += x;
--- loops/l3.js
+++ loops/l3.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var a = [1, 2, 3, 4, 5, 6];
-var s = 0;
-var p = 1;
-
-for (var i = 0; i < a.length; i += 1) {
-    s += a[i];
-    p *= a[i];
-=======
 var array = [1, 2, 3, 4, 5, 6];
 var s = 0;
 var p = 1;
@@ -15,6 +6,5 @@ var i;
 for (i = 0; i < array.length; i += 1) {
     s += array[i];
     p *= array[i];
->>>>>>> 24264f19f7f5198df4d35a476abd46d91c7b05cb
 }
 console.log('Sum : ' + s + ' Product :  ' + p); 
